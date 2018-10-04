e5b0495f1edc67011c5004e3f9f1e273fc0e7f79
Usuario <Usuario@10.22.148.96>
2018-10-04 13:07:08 +0100

    Kata2 v3 En esta versión se implementó un iterator

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,17 +1,17 @@
 package kata2;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Kata2 {
    
     public static void main(String[] args) {
         int[] array = {1,2,1,3,4,1,5,2,4,3,1,7,1,2,4,5,3};
-        Map<Integer,Integer> histogram = new HashMap<>();
-        for (int i : array) {
-            histogram.put(i, histogram.containsKey(i)? histogram.get(i)+1:1);
-        }
-        for (Map.Entry<Integer,Integer> entry : histogram.entrySet()) {
+        Histogram histo = new Histogram(array);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        Iterator<Map.Entry<Integer, Integer>> it = histogr.entrySet().iterator();
+        while(it.hasNext()) {
+            Map.Entry<Integer,Integer> entry = it.next();
             System.out.println(entry.getKey()+" ==> "+ entry.getValue());
         }
     }
