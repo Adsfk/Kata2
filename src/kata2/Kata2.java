6b30587ffcd908b82e4b2990cb6dd1b7aef96515
Usuario <Usuario@10.22.148.96>
2018-10-04 13:12:23 +0100

    Kata2 v4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Kata2 {
    
     public static void main(String[] args) {
-        int[] array = {1,2,1,3,4,1,5,2,4,3,1,7,1,2,4,5,3};
+        Integer[] array = {1,2,1,3,4,1,5,2,4,3,1,7,1,2,4,5,3};
         Histogram histo = new Histogram(array);
         Map<Integer,Integer> histogr = histo.getHistogram();
         Iterator<Map.Entry<Integer, Integer>> it = histogr.entrySet().iterator();
